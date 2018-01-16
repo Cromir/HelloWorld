020122b28f2a82c825ad584e7e26cfa6b6d98aa0
Giulio Bider <giulio.bider@gmail.com>
2018-01-16 14:14:46 +0100

    Update travis.yml and script

--- scripts/travis_instrumentation.sh
+++ scripts/travis_instrumentation.sh
@@ -5,13 +5,13 @@ set -e
 echo "Starting emulator for $COMPONENT tests"
 ./scripts/travis_create_emulator.sh &
 
-./gradlew :instrumentation:assembleDebug \
-  :instrumentation:assembleDebugAndroidTest \
+./gradlew assembleDebug \
+  assembleDebugAndroidTest \
   --parallel \
   -PDISABLE_ERROR_PRONE
 
 echo "Waiting for emulator..."
 android-wait-for-emulator
 
-for i in {1..3}; do ./gradlew :instrumentation:connectedDebugAndroidTest && break; done
+for i in {1..3}; do ./gradlew connectedDebugAndroidTest && break; done
 
